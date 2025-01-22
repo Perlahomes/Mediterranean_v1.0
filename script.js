502feb6bcf6d0d8f897480ae6d0a1cf906c7c30e
Perlahomes <perlagithub@gmail.com>
2025-01-22 18:01:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeLanguage() {
+    const language = document.getElementById('language').value;
+    alert("Language changed to: " + language); // Handle actual language change here.
+}
